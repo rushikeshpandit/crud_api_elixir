5ae088917fbd2a76944291c769f0d93c25ac3046
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-09-19 22:47:28 +0530

    - Code cleanup

--- lib/crud_api_elixir.ex
+++ lib/crud_api_elixir.ex
@@ -1,18 +1,2 @@
 defmodule CrudApiElixir do
-  @moduledoc """
-  Documentation for `CrudApiElixir`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CrudApiElixir.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
